b3a3fbd1aa544986b7a48bd03bf3904dd8c14694
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-15 10:59:51 +0800

    Create render.py

---
+++ render.py
@@ -0,0 +1,20 @@
+from flask import *
+from flaskext.markdown import Markdown
+app = Flask(__name__)
+Markdown(app)
+
+@app.route('/')
+def login():
+    return render_template('index.html')
+
+@app.route('/getfile', methods=['GET', 'POST'])
+def id():
+    if request.method == 'POST':
+        file=request.form['file']
+        f=open(f'files/{file}', 'rb')
+        return render_tenplate('index.html',content=str(f.read().decode()))
+    else:
+        return "Method not allow"
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=80)
